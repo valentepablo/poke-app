401d0df4edb5fe5cc7e063fd4493b19487efe781
Pablo Valente <pablo.nvalente@gmail.com>
2022-10-29 01:25:41 -0300

    fixed home route

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
           <Routes>
             <Route path="/myteam" element={<MyTeam />} />
             <Route
-              path="/pokemons"
+              path="/"
               element={
                 <PokemonList
                   page={page}
