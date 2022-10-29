beba80c694d438678c80c5f4667ad5302d363ceb
Pablo Valente <pablo.nvalente@gmail.com>
2022-10-29 01:26:10 -0300

    fixed home route 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 border-b-4 border-zinc-900 bg-white p-4 md:mb-4 ">
       <div className="mx-auto flex items-center justify-between md:max-w-5xl">
-        <Link to="pokemons">
+        <Link to="/">
           <button>Pokemons</button>
         </Link>
         <Link to="myteam">
